228b96dac80269cc944b782e3b8279d593bc4e41
Zetyn <rost.zgyrsk@gmail.com>
2023-05-25 23:35:53 +0300

    Initial commit

---
+++ Common/src/main/java/CommonClass.java
@@ -0,0 +1,5 @@
+public class CommonClass {
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ M2/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CommonClass commonClass = new CommonClass();
+        System.out.println(commonClass.hello() + " from M2");
+    }
+}
